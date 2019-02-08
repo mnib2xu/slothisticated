2cbbb598cf87c8676c757e6a96c5b980063c78fb
Robin <hahne.robin@gmail.com>
2019-02-08 17:22:27 +0100

    add basic levels

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 // Game 1 
 // Choose the right object
 function Game() {
-  this.object1 = $("#object1");
-  this.object2 = $("#object2");
-  this.object3 = $("#object3");
-  this.object4 = $("#object4");
+  this.objects = [$("#object1"),$("#object2"),$("#object3"),$("#object4")]
   this.colorArray = ["orange", "lightblue", "lightgreen", "grey"];
   this.randomColor = "";
   this.questionType = "";
   this.slothicles = 3;
   this.score = 0;
+  this.level = 3;
 
   // Fisher-Yates Shuffle
   this.shuffleArray = function(array) {
@@ -30,7 +28,14 @@ function Game() {
 
   this.createQuestion = function() {
     this.randomColor = this.getRandomItem(this.colorArray);
-    this.questionType = this.getRandomItem(["text","bg","border"])
+    var types = ["bg"];
+    if (this.level > 1){
+      types.push("border")
+    }
+    if (this.level > 2){
+      types.push("text")
+    }
+    this.questionType = this.getRandomItem(types)
     switch (this.questionType) {
       case "text":
         var question = "Click the circle called " + this.randomColor;
@@ -48,43 +53,52 @@ function Game() {
   }
 
   this.create = function() {
-    // fill text
-    var textColorArray = this.shuffleArray(this.colorArray);
-    this.object1.text(textColorArray[0]);
-    this.object2.text(textColorArray[1]);
-    this.object3.text(textColorArray[2]);
-    this.object4.text(textColorArray[3]);
-
-    // fill bg Color
+    
+    // fill background color
     var backgroundColorArray = this.shuffleArray(this.colorArray);
-    this.object1.css("background-color",backgroundColorArray[0]);
-    this.object2.css("background-color",backgroundColorArray[1]);
-    this.object3.css("background-color",backgroundColorArray[2]);
-    this.object4.css("background-color",backgroundColorArray[3]);
-
-    // fill border Color
-    var borderColorArray = this.shuffleArray(this.colorArray);
-    this.object1.css("border-color",borderColorArray[0]);
-    this.object2.css("border-color",borderColorArray[1]);
-    this.object3.css("border-color",borderColorArray[2]);
-    this.object4.css("border-color",borderColorArray[3]);
-
+    this.objects.forEach(function(element,index) {
+      element.css("background-color",backgroundColorArray[index]);
+    });
+    if (this.level > 1){
+      // fill border color
+      var borderColorArray = this.shuffleArray(this.colorArray);
+      this.objects.forEach(function(element,index) {
+        element.css("border", `${borderColorArray[index]} 10px solid`);
+      });
+    }
+    if (this.level > 2){
+      // fill text
+      var textColorArray = this.shuffleArray(this.colorArray);
+      this.objects.forEach(function(element,index) {
+        element.text(textColorArray[index]);
+      });
+    }
+   
     $("#slothicles").text(this.slothicles)
     $("#score").text(this.score)
     this.createQuestion();
+    this.timer(30);
+  }
+
+  this.timer = function(sec) {
+    var timer = setInterval(function(){
+        document.getElementById('time').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+            alert(myGame.score)
+        }
+    }, 1000);
   }
 
   this.checkAnswer = function(text, bg, border) {
     switch (this.questionType) {
       case "text":
         return (text === this.randomColor)
-        break;
       case "bg":
         return (bg === this.randomColor)
-        break;
       case "border":
         return (border === this.randomColor)
-        break;
       default:
         break;
     }
@@ -94,29 +108,16 @@ function Game() {
 $(document).ready(function () {
   let myGame = new Game();
 
-  function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('time').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-            alert(myGame.score)
-        }
-        console.log(sec)
-        console.log(timer)
-    }, 1000);
-  }
-  timer();
-
-  myGame.create();
+  $("#start-game").click(function(event){
+    myGame.create();
+  })
 
   $(".object").click(function(event){
     var object = event.currentTarget;
-    if (!myGame.checkAnswer(object.textContent, object.style.backgroundColor ,object.style.borderColor)){
-      myGame.slothicles -= 1;
-    }else{
+    if (myGame.checkAnswer(object.textContent, object.style.backgroundColor ,object.style.borderColor)){
       myGame.score++;
+    }else{
+      myGame.slothicles -= 1;
     }
     $("#slothicles").text(myGame.slothicles)
     $("#score").text(myGame.score)
