60d1506212b8a1489ed11275411da2c49aa1062a
Robin <hahne.robin@gmail.com>
2019-02-13 15:22:14 +0100

    simplify code part2

--- js/master.js
+++ js/master.js
@@ -10,32 +10,17 @@ class MasterGame {
 
   decreaseLive(previousGame) {
     $(".live-bar ul li:last-child").remove();
-    if (this.__proto__.__proto__.slothicles === 1){
-      $("#end-screen").css("display","flex");
+    if (this.__proto__.__proto__.slothicles === 1) {
+      $("#end-screen").css("display", "flex");
       $("#random-question").text("The end!");
-    }
-    else {
+    } else {
       this.__proto__.__proto__.slothicles -= 1;
-      $("#countdown").css("display","flex");
+      if (previousGame === "game2") {
+        $("#reaction-result").css("display", "block");
+      }
       $("#random-question").text("You Failed!");
-      $(".question-area").css("background-color","rgb(212, 156, 168)")
-      if(previousGame === "game2"){
-        debugger
-        $("#reaction-result").css("display","block");
-      }
-      var timeleft = 3;
-      $("#get-ready").text(timeleft + "...");
-      var downloadTimer = setInterval(function(){
-      timeleft--;
-      $("#get-ready").text(timeleft + "...");
-      if(timeleft <= 0){
-        clearInterval(downloadTimer);
-        $("#countdown").css("display","none");
-        $(".question-area").css("background-color","rgb(86, 52, 170)")
-        $("#reaction-result").css("display","none");
-        this.loadRandomGame();
-      }
-      }.bind(this),1000);
+      $(".question-area").css("background-color", "rgb(212, 156, 168)")
+      this.getGetGreadyCountDown();
     }
   }
 
@@ -48,28 +33,32 @@ class MasterGame {
     } else if (this.__proto__.__proto__.score > 4) {
       this.__proto__.level = 2;
     }
-    if(previousGame === "game2"){
-      debugger
-      $("#reaction-result").css("display","block");
+    if (previousGame === "game2") {
+      $("#reaction-result").css("display", "block");
     }
     $("#score").text(this.__proto__.__proto__.score);
     $("#level").text(this.__proto__.level);
-    $("#countdown").css("display","flex");
+
     $("#random-question").text("You passed!");
-    $(".question-area").css("background-color","rgb(103, 154, 168)")
+    $(".question-area").css("background-color", "rgb(103, 154, 168)")
+    this.getGetGreadyCountDown();
+  }
+
+  getGetGreadyCountDown() {
+    $("#countdown").css("display", "flex");
     var timeleft = 3;
     $("#get-ready").text(timeleft + "...");
-    var downloadTimer = setInterval(function(){
-    timeleft--;
-    $("#get-ready").text(timeleft + "...");
-    if(timeleft <= 0){
-      clearInterval(downloadTimer);
-      $("#countdown").css("display","none");
-      $(".question-area").css("background-color","rgb(86, 52, 170)")
-      $("#reaction-result").css("display","none");
-      this.loadRandomGame();
-    }
-    }.bind(this),1000);
+    var downloadTimer = setInterval(function () {
+      timeleft--;
+      $("#get-ready").text(timeleft + "...");
+      if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        $("#countdown").css("display", "none");
+        $(".question-area").css("background-color", "rgb(86, 52, 170)")
+        $("#reaction-result").css("display", "none");
+        this.loadRandomGame();
+      }
+    }.bind(this), 1000);
   }
 
   init() {
@@ -83,9 +72,9 @@ class MasterGame {
 
   loadRandomGame() {
     var gameArray = [];
-    if (this.__proto__.allGames.length > 0){
+    if (this.__proto__.allGames.length > 0) {
       gameArray = this.__proto__.allGames;
-    }else if(this.__proto__.__proto__.allGames.length > 0){
+    } else if (this.__proto__.__proto__.allGames.length > 0) {
       gameArray = this.__proto__.__proto__.allGames;
     }
     gameArray[0].remove();
@@ -136,28 +125,28 @@ $(document).ready(function () {
 
 
   // Start
-  $(".start-the-game").click(function() {
+  $(".start-the-game").click(function () {
     createPatterns();
 
-    $("#menu").css("display","none");
-    $("#countdown").css("display","flex");
+    $("#menu").css("display", "none");
+    $("#countdown").css("display", "flex");
 
-    $(".live-bar").css("display","flex");
-    $("footer").css("display","flex");
+    $(".live-bar").css("display", "flex");
+    $("footer").css("display", "flex");
 
     var timeleft = 3;
     $("#get-ready").text(timeleft + "...");
-    var downloadTimer = setInterval(function(){
-    createPatterns();
-    timeleft--;
-    $("#get-ready").text(timeleft + "...");
-    if(timeleft <= 0){
-      clearInterval(downloadTimer);
-      $("#countdown").css("display","none");
-      removePatterns();
-      brain.init();
-    }
-    },1000);
+    var downloadTimer = setInterval(function () {
+      createPatterns();
+      timeleft--;
+      $("#get-ready").text(timeleft + "...");
+      if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        $("#countdown").css("display", "none");
+        removePatterns();
+        brain.init();
+      }
+    }, 1000);
   })
 
   // Game ONE
@@ -178,17 +167,21 @@ function createPatterns() {
   $(".patterns").children().remove();
   var windowWidth = $(window).width();
   var colorArray = ["rgb(212, 156, 168)", "rgb(86, 52, 170)", "rgb(86, 52, 170)", "rgb(103, 154, 168)", "rgb(248, 60, 167)"];
-  var patternArray = [['<div class="pattern-circle"></div>','<div class="pattern-triangle-upside-down"></div>',''],['<div class="pattern-circle"></div>','<div class="pattern-triangle"></div>','']];
+  var patternArray = [
+    ['<div class="pattern-circle"></div>', '<div class="pattern-triangle-upside-down"></div>', ''],
+    ['<div class="pattern-circle"></div>', '<div class="pattern-triangle"></div>', '']
+  ];
   var amountOfPatternBoxes = Math.floor(windowWidth / 80);
-  for (var i = 0; i < 2; i++){
-    for (var j = 0; j < amountOfPatternBoxes; j++){
+  for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < amountOfPatternBoxes; j++) {
       $(".patterns").append('<div class="pattern-box"></div>');
       $(".patterns").children(":last-child").append(patternArray[i][Math.floor(Math.random() * patternArray[i].length)])
-      $(".patterns").children(":last-child").children().css("background-color",colorArray[Math.floor(Math.random() * colorArray.length)]);
-      $(".patterns").children(":last-child").css("background-color",colorArray[Math.floor(Math.random() * colorArray.length)]);
+      $(".patterns").children(":last-child").children().css("background-color", colorArray[Math.floor(Math.random() * colorArray.length)]);
+      $(".patterns").children(":last-child").css("background-color", colorArray[Math.floor(Math.random() * colorArray.length)]);
     }
   }
 }
+
 function removePatterns() {
   $(".patterns").children().remove();
 }
